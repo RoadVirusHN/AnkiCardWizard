7fbc4cb9e4ff6e8820342fcf0a0380ee7822030f
junseokyun <roadvirushn@gmail.com>
2025-10-28 14:54:41 +0000

    [refact] change fetchAnki to generic func.

--- src/popup/utils/fetchAnki.ts
+++ src/popup/utils/fetchAnki.ts
@@ -2,26 +2,18 @@ interface fetAnkiRequestBody {
   action: string;
   params?: Record<string, unknown>;
 }
-interface fetchAnkiResponseBody {
-  result: unknown;
+// TODO : change the type of result to a generic type T
+interface fetchAnkiResponseBody<T= unknown> {
+  result: T;
   error: string | null;
 }
-interface fetchAnkiReturnType {
-  data: fetchAnkiResponseBody | unknown;
-  error: Error | null;
-}
-async function fetchAnki(request: fetAnkiRequestBody): Promise<fetchAnkiReturnType> {
-  await fetch('http://127.0.0.1:8765', {
+const fetchAnki = async <T>(request: fetAnkiRequestBody): Promise<fetchAnkiResponseBody<T>> => {
+  const res = await fetch('http://127.0.0.1:8765', {
     method: 'POST',
     body: JSON.stringify({ ...request, version: 5 }),
-  })
-    .then(async (res) => {
-      const data: fetchAnkiResponseBody = await res.json();
-      return data;
-    })
-    .catch((err) => {
-      throw err;
-    });
-  return { data: null, error: null };
+  }).catch((err) => {
+    throw err;
+  });
+  return res.json();
 }
 export default fetchAnki;
