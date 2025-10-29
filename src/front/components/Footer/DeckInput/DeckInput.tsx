b421bda4c6815247861bf7709579e14ef615ba2a
junseokyun <roadvirushn@gmail.com>
2025-10-29 15:10:28 +0000

    [front] change DeckInput

--- src/front/components/Footer/DeckInput/DeckInput.tsx
+++ src/front/components/Footer/DeckInput/DeckInput.tsx
@@ -1,11 +1,12 @@
+import useAnkiConnectionStore from "@/front/utils/useAnkiConnectionStore";
 import DecksSvg from "@/public/Decks-Svg.svg";
 const DeckInput = ({}) => {
-  
+  const {decks} = useAnkiConnectionStore();
   return (
-    <div>
-      <DecksSvg width={20} height={20}/>
-      <select id="deck-select" name="deck-select">
-        <option></option>
+    <div style={{display: 'flex', alignItems: 'center', gap: '5px', height: '100%'}}>
+      <DecksSvg width={18} height={18} style={{ display: 'block', flexShrink: 0 }}/>
+      <select id="deck-select" name="deck-select" style={{height: '20px', width: '180px', padding: '0 4px'}}>
+        {decks.length>0? decks.map((deck) => <option key={deck} value={deck}>{deck}</option>) : <option>Check Anki Connection!</option>}
       </select>
     </div>
   );
