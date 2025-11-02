d1cf6cfd4ddc739bbe9ae537b3e929ad684ee6b5
junseokyun <roadvirushn@gmail.com>
2025-11-02 06:27:49 +0000

    [front] add card page & DetectedCard comp

--- src/front/components/pages/Cards/CardPage.tsx
+++ src/front/components/pages/Cards/CardPage.tsx
@@ -1,47 +1,42 @@
-import commonStyle from '@/front/common.module.css';
-import useAnkiConnectionStore from '@/front/utils/useAnkiConnectionStore';
-import useGlobalVarStore from '@/front/utils/useGlobalVarStore';
-import { useRef } from 'react';
+import cardPageStyles from '@/front/components/pages/Cards/cardPage.module.css';
+import { useEffect, useRef, useState } from 'react';
 import Footer from '../../Footer/Footer';
+import useCustomCard from '@/front/utils/useCustomCard';
+import DetectedCard from './DetectedCard/DetectedCard';
+
+export interface Extracted{
+  Front : Record<string, string>;
+  Back : Record<string, string>;
+}
+interface IdxedExtracted{
+  customCardIndex: number,
+  extracted: Extracted
+};
+
 
 const CardPage: React.FC = () => {
-  const frontRef = useRef<HTMLTextAreaElement>(null);
-  const backRef = useRef<HTMLTextAreaElement>(null);
-  const {fetchAnki} = useAnkiConnectionStore();
-  const {currentDeck} = useGlobalVarStore();
-  const addToAnki = () => {
-    fetchAnki<{noteIds: number[]}>({
-      action: 'addNote',
-      params: {
-        note: {
-          deckName: currentDeck || 'Default',
-          modelName: 'Basic',
-          fields: {
-            Front: frontRef.current?.value || 'Sample Front',
-            Back:  backRef.current?.value || 'Sample Back',
-          },
-          options: {
-            allowDuplicate: false,
-          },
-          tags: ['anki-card-wizard'],
-        },
-      },
-    }).then((res) => {
-      if (res.error) {
-        console.error('Error adding note to Anki:', res.error);
-        alert('Failed to add note to Anki: ' + res.error);
-      } else {
-        console.log('Note added to Anki with IDs:', res.result?.noteIds);
-        alert('Note added to Anki successfully!');
+  const {customCards} = useCustomCard();
+  const [extracteds, setExtracteds] = useState<IdxedExtracted[]>([]);
+  const requestExtracteds = () =>{
+    chrome.runtime.sendMessage({type: 'SEARCH_DETECTED_CARDS', customCards});
+  }
+  useEffect(()=>{
+    chrome.runtime.onMessage.addListener((message)=>{
+      if (message.type === 'URL_CHANGED') {
+        chrome.runtime.sendMessage({type: 'SEARCH_DETECTED_CARDS', customCards});
+      } else if (message.type === 'CARDS_DETECTED_UPDATE_REQUEST') {
+        setExtracteds(message.extracteds);
       }
     });
-  };  
 
+    if (customCards.length > 0) {
+      chrome.runtime.sendMessage({type: 'REQUEST_DETECTED_CARDS', customCards});
+    }
+  },[customCards])
   return (
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: '8px'}}>
-      <textarea ref={frontRef} placeholder="Front"/>
-      <textarea ref={backRef} placeholder="Back"/>
-      <button onClick={()=>addToAnki()}>add</button>
+      {extracteds.map(({customCardIndex, extracted}, idx)=><DetectedCard key={idx} customCard={customCards[customCardIndex]} extracted={extracted}/>)}
+      <span className={cardPageStyles.redetectCard} onClick={requestExtracteds}> ↺ </span>
       <Footer/>
     </div>
   );
---
+++ src/front/components/pages/Cards/DetectedCard/DetectedCard.tsx
@@ -0,0 +1,89 @@
+import useAnkiConnectionStore from "@/front/utils/useAnkiConnectionStore";
+import { CardFieldDataType, CustomCard } from "@/front/utils/useCustomCard";
+import useGlobalVarStore from "@/front/utils/useGlobalVarStore";
+import cardPageStyles from "@/front/components/pages/Cards/cardPage.module.css";
+import { Extracted } from "../CardPage";
+
+const DetectedCard = ({customCard, extracted}:{customCard: CustomCard, extracted: Extracted}) => {
+  const {fetchAnki} = useAnkiConnectionStore();
+  const {currentDeck} = useGlobalVarStore();
+  const addToAnki = () => {
+  const buildedFront = customCard.Front.html.replaceAll(/\{\{(.*?)\}\}/g, 
+    (_, fieldName) => {
+      const fieldInfo = customCard.Front.fields.find(f=>f.name === fieldName);
+      if (fieldInfo) {
+        switch (fieldInfo.dataType) {
+          case CardFieldDataType.IMAGE:
+            return `<img src="${extracted.Front[fieldName] || ''}" />`;
+          case CardFieldDataType.AUDIO:
+            return `[sound:${extracted.Front[fieldName] || ''}]`;
+          case CardFieldDataType.TEXT:
+            return extracted.Front[fieldName] || '';
+          default:
+            return extracted.Front[fieldName] || '';
+        }
+      }
+      return ''; // Ensure a string is always returned
+    });
+  const buildedBack = customCard.Back.html.replaceAll(/\{\{(.*?)\}\}/g, 
+    (_, fieldName) => {
+      const fieldInfo = customCard.Back.fields.find(f=>f.name === fieldName);
+      if (fieldInfo) {
+        switch (fieldInfo.dataType) {
+          case CardFieldDataType.IMAGE:
+            return `<img src="${extracted.Back[fieldName] || ''}" />`;
+          case CardFieldDataType.AUDIO:
+            return `[sound:${extracted.Back[fieldName] || ''}]`;
+          case CardFieldDataType.TEXT:
+            return extracted.Back[fieldName] || '';
+          default:
+            return extracted.Back[fieldName] || '';
+        }
+      }
+      return '';
+    }
+  );
+
+    fetchAnki<{noteIds: number[]}>({
+      action: 'addNote',
+      params: {
+        note: {
+          deckName: currentDeck || 'Default',
+          modelName: customCard.modelName || 'Basic',
+          fields: {
+            Front: buildedFront || 'Something Wrong with Front',
+            Back:  buildedBack || 'Something Wrong with Back',
+          },
+          audio: customCard.audio ? [{
+            url: customCard.audio.url,
+            filename: customCard.audio.filename,
+            skipHash: customCard.audio.skipHash,
+            fields: customCard.audio.fields,
+          }] : [],
+          tags: customCard.tags || [],
+        },
+      },
+    }).then((res) => {
+      if (res.error) {
+        console.error('Error adding note to Anki:', res.error);
+        alert('Failed to add note to Anki: ' + res.error);
+      } else {
+        console.log('Note added to Anki with ID:', res.result);
+        alert('Note added to Anki successfully!');
+      }
+    });
+  };  
+  const AddCard = () => {
+    addToAnki();
+  }
+
+  return (<div className={cardPageStyles.detectedCardContainer}>  
+    <div className={cardPageStyles.detectedCardContent}>
+      card : <h3>{customCard.cardName}</h3>
+      front : <p>{extracted.Front['front']}</p>      
+      back : <p>{extracted.Back['back']}</p>      
+    </div>
+    <div onClick={AddCard} style={{cursor: 'pointer', userSelect: "none"}}>⨁</div>
+  </div>);
+};
+export default DetectedCard;
